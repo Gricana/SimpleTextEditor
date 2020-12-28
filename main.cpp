7f0f50e90e33ef4073102ac8676211372518ca3e
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2020-12-28 17:02:53 +0300

    finished

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("TextEditor");
-    a.setApplicationVersion(QString("%1.%2").arg(MAJOR_VERSION).arg(MINOR_VERSION));
+    a.setApplicationVersion(QString("%1.%2").arg(MAJ_VERSION).arg(MIN_VERSION));
     MainWindow w;
     w.show();
     return a.exec();
