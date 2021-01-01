93ee091961e710aa6778205e9234903bef08734d
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-01-01 20:28:38 +0300

    trying to add allowing changing theme of application

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -62,6 +62,20 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent),
     connect(ui->action_Background, SIGNAL(triggered()), this, SLOT(slotSetColorBackground()), Qt::UniqueConnection);
     slotNew();
     ui->plainTextEdit->setFont(QFont("Times", 14));
+    QPalette p = palette();
+    p.setColor(QPalette::Window, QColor("#2e2f30"));
+    p.setColor(QPalette::WindowText, Qt::white);
+    p.setColor(QPalette::ButtonText, Qt::white);
+    p.setColor(QPalette::HighlightedText, QColor("#2e2f30"));
+    p.setColor(QPalette::AlternateBase, Qt::black);
+    ui->menubar->setStyleSheet(QString("background-color: #2e2f30; color: white;"));
+    ui->plainTextEdit->setStyleSheet(QString("background-color: #2e2f30; color: white;"));
+    ui->menu_File->setStyleSheet("QMenu::item::selected { background-color: #3c5670; }");
+    ui->menu_Edit->setStyleSheet("QMenu::item::selected { background-color: #3c5670; }");
+    ui->menu_Format->setStyleSheet("QMenu::item::selected { background-color: #3c5670; }");
+    ui->menu_About->setStyleSheet("QMenu::item::selected { background-color: #3c5670; }");
+    ui->menu_Color->setStyleSheet("QMenu::item::selected { background-color: #3c5670; }");
+    setPalette(p);
 }
 
 void MainWindow::updateTitle()
